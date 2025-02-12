d7e7cc740fdfe64e7449c15d7ba9d2440a354b0a
cchjimmy <92587458+cchjimmy@users.noreply.github.com>
2025-02-12 02:57:40 +0000

    added speed slider and pause/play

--- build/out.js
+++ build/out.js
@@ -1,124 +1,131 @@
 (() => {
   // src/main.ts
-  (function() {
-    const canvas = document.querySelector("canvas");
-    const ctx = canvas.getContext("2d");
-    const clearButton = document.querySelector("button");
-    if (!ctx)
+  var canvas = document.querySelector("canvas");
+  var ctx = canvas.getContext("2d");
+  var clearButton = document.querySelector("#clear");
+  var pauseButton = document.querySelector("#pause");
+  var speedRange = document.querySelector("#speed");
+  var speedSpan = document.querySelector("span");
+  var rules = [
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0]
+  ];
+  var w = 100;
+  var h = 100;
+  var scale = 5;
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = `${w * scale}px`;
+  canvas.style.height = `${h * scale}px`;
+  canvas.style.position = "absolute";
+  canvas.style.top = "50%";
+  canvas.style.left = "50%";
+  canvas.style.transform = "translate(-50%, -50%)";
+  canvas.style.background = "#000000";
+  canvas.style.touchAction = "none";
+  var board = [];
+  for (let i = 0; i < h; ++i) {
+    board[i] = [];
+    for (let j = 0; j < w; ++j) {
+      board[i][j] = 0;
+    }
+  }
+  var isPointerDown = false;
+  var x;
+  var y;
+  window.onpointerdown = () => isPointerDown = true;
+  window.onpointerup = () => isPointerDown = false;
+  window.onpointermove = (e) => {
+    let computedStyleCanvas = getComputedStyle(canvas);
+    let canvasTop = (innerHeight - parseFloat(computedStyleCanvas.height)) / 2;
+    let canvasLeft = (innerWidth - parseFloat(computedStyleCanvas.width)) / 2;
+    x = (e.x - canvasLeft) / scale;
+    y = (e.y - canvasTop) / scale;
+    x = Math.round(x);
+    y = Math.round(y);
+    if (!isPointerDown || x < 0 || x > w - 1 || y < 0 || y > h - 1)
       return;
-    const rules = [
-      [0, 0, 0, 1, 0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 0, 0, 0, 0, 0]
-    ];
-    const w = 100;
-    const h = 100;
-    const scale = 5;
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.width = `${w * scale}px`;
-    canvas.style.height = `${h * scale}px`;
-    canvas.style.position = "absolute";
-    canvas.style.top = "50%";
-    canvas.style.left = "50%";
-    canvas.style.transform = "translate(-50%, -50%)";
-    canvas.style.background = "#000000";
-    canvas.style.touchAction = "none";
-    let board = [];
-    for (let i = 0; i < h; ++i) {
-      board[i] = [];
-      for (let j = 0; j < w; ++j) {
-        board[i][j] = 0;
-      }
-    }
-    let isPointerDown = false;
-    let x;
-    let y;
-    window.onpointerdown = (e) => {
-      isPointerDown = true;
-    };
-    window.onpointerup = () => {
-      isPointerDown = false;
-    };
-    window.onpointermove = (e) => {
-      if (!isPointerDown)
-        return;
-      let computedStyleCanvas = getComputedStyle(canvas);
-      let canvasTop = (innerHeight - parseFloat(computedStyleCanvas.height)) / 2;
-      let canvasLeft = (innerWidth - parseFloat(computedStyleCanvas.width)) / 2;
-      x = (e.x - canvasLeft) / scale;
-      y = (e.y - canvasTop) / scale;
-      x = Math.round(x);
-      y = Math.round(y);
-      x = clamp(x, 0, w - 1);
-      y = clamp(y, 0, h - 1);
-      board[y][x] = 1;
-    };
-    clearButton.onclick = () => clearBoard(board);
-    setInterval(() => {
+    x = clamp(x, 0, w - 1);
+    y = clamp(y, 0, h - 1);
+    board[y][x] = 1;
+  };
+  clearButton.onclick = () => clearBoard(board);
+  var stepSpeed = 150;
+  speedSpan.innerText = `${stepSpeed}`;
+  var pause = false;
+  pauseButton.onclick = () => pause = !pause;
+  speedRange.onchange = (e) => {
+    let speed = e.target.value;
+    stepSpeed = parseFloat(speed);
+    speedSpan.innerText = speed;
+  };
+  var last = performance.now();
+  function loop(ctx2) {
+    let time = performance.now() - last;
+    draw(board, ctx2);
+    ctx2.fillStyle = "#FF0000";
+    ctx2.fillRect(x, y, 1, 1);
+    if (!pause && time > stepSpeed) {
       board = stepSlidingWindow(board, rules);
-    }, 150);
-    function loop(ctx2) {
-      draw(board, ctx2);
-      ctx2.fillStyle = "#FF0000";
-      ctx2.fillRect(x, y, 1, 1);
-      requestAnimationFrame(() => loop(ctx2));
+      last = performance.now();
     }
+    requestAnimationFrame(() => loop(ctx2));
+  }
+  if (ctx)
     loop(ctx);
-  })();
-  function stepSlidingWindow(board, rules) {
-    const h = board.length;
-    const w = board[0].length;
-    const out = new Array(h);
-    for (let i = 0; i < h; ++i) {
-      out[i] = new Array(w);
+  function stepSlidingWindow(board2, rules2) {
+    const h2 = board2.length;
+    const w2 = board2[0].length;
+    const out = new Array(h2);
+    for (let i = 0; i < h2; ++i) {
+      out[i] = new Array(w2);
       let neighbours = 0;
       let yStart = i - 1;
       yStart = yStart < 0 ? 0 : yStart;
       let yEnd = i + 1;
-      yEnd = yEnd >= h ? h - 1 : yEnd;
+      yEnd = yEnd >= h2 ? h2 - 1 : yEnd;
       for (let k = yStart; k <= yEnd; ++k) {
         for (let l = 0; l < 2; ++l) {
-          neighbours += board[k][l];
+          neighbours += board2[k][l];
         }
       }
-      for (let j = 0; j < w; ++j) {
-        let state = board[i][j];
-        out[i][j] = rules[state][neighbours - state];
-        let canAdd = j + 2 < w;
+      for (let j = 0; j < w2; ++j) {
+        let state = board2[i][j];
+        out[i][j] = rules2[state][neighbours - state];
+        let canAdd = j + 2 < w2;
         let canSub = j - 1 >= 0;
         for (let m = yStart; m <= yEnd; ++m) {
-          canAdd && (neighbours += board[m][j + 2]);
-          canSub && (neighbours -= board[m][j - 1]);
+          canAdd && (neighbours += board2[m][j + 2]);
+          canSub && (neighbours -= board2[m][j - 1]);
         }
       }
     }
     return out;
   }
-  function draw(board, ctx, aliveColor = "#FFFF00") {
-    const h = board.length;
-    const w = board[0].length;
-    let oldStyle = ctx.fillStyle;
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = aliveColor;
-    ctx.beginPath();
-    for (let i = 0; i < h; ++i) {
-      for (let j = 0; j < w; ++j) {
-        let state = board[i][j];
-        if (!state)
+  function draw(board2, ctx2, aliveColor = "#FFFF00") {
+    const h2 = board2.length;
+    const w2 = board2[0].length;
+    let oldStyle = ctx2.fillStyle;
+    ctx2.clearRect(0, 0, ctx2.canvas.width, ctx2.canvas.height);
+    ctx2.fillStyle = aliveColor;
+    ctx2.beginPath();
+    for (let i = 0; i < h2; ++i) {
+      for (let j = 0; j < w2; ++j) {
+        if (!board2[i][j])
           continue;
-        ctx.rect(j, i, 1, 1);
+        ctx2.rect(j, i, 1, 1);
       }
     }
-    ctx.fill();
-    ctx.fillStyle = oldStyle;
+    ctx2.fill();
+    ctx2.fillStyle = oldStyle;
   }
-  function clamp(x, min, max) {
-    return Math.min(Math.max(x, min), max);
+  function clamp(x2, min, max) {
+    return Math.min(Math.max(x2, min), max);
   }
-  function clearBoard(board) {
-    for (let i = 0; i < board.length; ++i) {
-      for (let j = 0; j < board[i].length; ++j) {
-        board[i][j] = 0;
+  function clearBoard(board2) {
+    for (let i = 0; i < board2.length; ++i) {
+      for (let j = 0; j < board2[i].length; ++j) {
+        board2[i][j] = 0;
       }
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -1,183 +1,167 @@
-(function () {
-  const canvas = <HTMLCanvasElement>document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
-  const clearButton = <HTMLButtonElement>document.querySelector("button");
+const canvas = <HTMLCanvasElement>document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const clearButton = <HTMLButtonElement>document.querySelector("#clear");
+const pauseButton = <HTMLButtonElement>document.querySelector("#pause");
+const speedRange = <HTMLInputElement>document.querySelector("#speed");
+const speedSpan = <HTMLSpanElement>document.querySelector("span");
 
-  if (!ctx) return;
+const rules = [
+	[0, 0, 0, 1, 0, 0, 0, 0, 0],
+	[0, 0, 1, 1, 0, 0, 0, 0, 0],
+];
 
-  const rules = [
-    [0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0],
-  ];
+const w = 100;
+const h = 100;
+const scale = 5;
 
-  const w = 100;
-  const h = 100;
+canvas.width = w;
+canvas.height = h;
+canvas.style.width = `${w * scale}px`;
+canvas.style.height = `${h * scale}px`;
+canvas.style.position = "absolute";
+canvas.style.top = "50%";
+canvas.style.left = "50%";
+canvas.style.transform = "translate(-50%, -50%)";
+canvas.style.background = "#000000";
+canvas.style.touchAction = "none";
 
-  const scale = 5;
+let board: number[][] = [];
 
-  canvas.width = w;
-  canvas.height = h;
-  canvas.style.width = `${w * scale}px`;
-  canvas.style.height = `${h * scale}px`;
-  canvas.style.position = "absolute";
-  canvas.style.top = "50%";
-  canvas.style.left = "50%";
-  canvas.style.transform = "translate(-50%, -50%)";
-  canvas.style.background = "#000000";
-
-  canvas.style.touchAction = "none";
-
-  let board: number[][] = [];
-
-  for (let i = 0; i < h; ++i) {
-    board[i] = [];
-    for (let j = 0; j < w; ++j) {
-      board[i][j] = 0;
-    }
-  }
-
-  let isPointerDown = false;
-
-  let x: number;
-  let y: number;
-
-  window.onpointerdown = (e) => {
-    isPointerDown = true;
-  };
-
-  window.onpointerup = () => {
-    isPointerDown = false;
-  };
-
-  window.onpointermove = (e: PointerEvent) => {
-    if (!isPointerDown) return;
-
-    let computedStyleCanvas = getComputedStyle(canvas);
-    let canvasTop = (innerHeight - parseFloat(computedStyleCanvas.height)) / 2;
-    let canvasLeft = (innerWidth - parseFloat(computedStyleCanvas.width)) / 2;
-
-    x = (e.x - canvasLeft) / scale;
-    y = (e.y - canvasTop) / scale;
-
-    x = Math.round(x);
-    y = Math.round(y);
-
-    x = clamp(x, 0, w - 1);
-    y = clamp(y, 0, h - 1);
-
-    board[y][x] = 1;
-  };
-
-  clearButton.onclick = () => clearBoard(board);
-
-  setInterval(() => {
-    board = stepSlidingWindow(board, rules);
-  }, 150);
-
-  function loop(ctx: CanvasRenderingContext2D) {
-    draw(board, ctx);
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(x, y, 1, 1);
-    requestAnimationFrame(() => loop(ctx));
-  }
-
-  loop(ctx);
-})();
-
-function step(board: number[][], rules: number[][]): number[][] {
-  const h = board.length;
-  const w = board[0].length;
-  const out: number[][] = new Array(h);
-  for (let i = 0; i < h; ++i) {
-    out[i] = new Array(w);
-    for (let j = 0; j < w; ++j) {
-      let neighbours = 0;
-      for (let k = i - 1; k < i + 2; ++k) {
-        for (let l = j - 1; l < j + 2; ++l) {
-          if (k == i && l == j) continue;
-          if (k >= 0 && k < h && l >= 0 && l < w) {
-            neighbours += board[k][l];
-          }
-        }
-      }
-      out[i][j] = rules[board[i][j]][neighbours];
-      // console.log(`x: ${j}, y: ${i}, neighbours: ${neighbours}`);
-    }
-  }
-  return out;
+for (let i = 0; i < h; ++i) {
+	board[i] = [];
+	for (let j = 0; j < w; ++j) {
+		board[i][j] = 0;
+	}
 }
 
+let isPointerDown = false;
+
+let x: number;
+let y: number;
+
+window.onpointerdown = () => isPointerDown = true;
+
+window.onpointerup = () => isPointerDown = false;
+
+window.onpointermove = (e: PointerEvent) => {
+	let computedStyleCanvas = getComputedStyle(canvas);
+	let canvasTop =
+		(innerHeight - parseFloat(computedStyleCanvas.height)) /
+		2;
+	let canvasLeft =
+		(innerWidth - parseFloat(computedStyleCanvas.width)) /
+		2;
+
+	x = (e.x - canvasLeft) / scale;
+	y = (e.y - canvasTop) / scale;
+
+	x = Math.round(x);
+	y = Math.round(y);
+
+	if (!isPointerDown || x < 0 || x > w - 1 || y < 0 || y > h - 1) return;
+	x = clamp(x, 0, w - 1);
+	y = clamp(y, 0, h - 1);
+	board[y][x] = 1;
+};
+
+clearButton.onclick = () => clearBoard(board);
+
+let stepSpeed = 150;
+speedSpan.innerText = `${stepSpeed}`;
+let pause = false;
+
+pauseButton.onclick = () => pause = !pause;
+
+speedRange.onchange = (e: Event) => {
+	let speed = (e.target as HTMLInputElement).value;
+	stepSpeed = parseFloat(speed);
+	speedSpan.innerText = speed;
+}
+
+let last = performance.now();
+function loop(ctx: CanvasRenderingContext2D) {
+	let time = performance.now() - last;
+	draw(board, ctx);
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(x, y, 1, 1);
+	if (!pause && time > stepSpeed) {
+		board = stepSlidingWindow(board, rules);
+		last = performance.now();
+	}
+	requestAnimationFrame(() => loop(ctx));
+}
+
+if (ctx) loop(ctx);
+
 function stepSlidingWindow(board: number[][], rules: number[][]): number[][] {
-  const h = board.length;
-  const w = board[0].length;
-  const out: number[][] = new Array(h);
-  for (let i = 0; i < h; ++i) {
-    out[i] = new Array(w);
-    let neighbours = 0;
-    let yStart = i - 1;
-    yStart = yStart < 0 ? 0 : yStart;
-    let yEnd = i + 1;
-    yEnd = yEnd >= h ? h - 1 : yEnd;
-    for (let k = yStart; k <= yEnd; ++k) {
-      for (let l = 0; l < 2; ++l) {
-        neighbours += board[k][l];
-      }
-    }
-    for (let j = 0; j < w; ++j) {
-      let state = board[i][j];
-      // console.log(`x: ${j}, y: ${i}, neighbours: ${neighbours - state}`);
-      out[i][j] = rules[state][neighbours - state];
-      let canAdd = j + 2 < w;
-      let canSub = j - 1 >= 0;
-      for (let m = yStart; m <= yEnd; ++m) {
-        canAdd && (neighbours += board[m][j + 2]);
-        canSub && (neighbours -= board[m][j - 1]);
-      }
-    }
-  }
-  return out;
+	const h = board.length;
+	const w = board[0].length;
+	const out: number[][] = new Array(h);
+	for (let i = 0; i < h; ++i) {
+		out[i] = new Array(w);
+		let neighbours = 0;
+		let yStart = i - 1;
+		yStart = yStart < 0 ? 0 : yStart;
+		let yEnd = i + 1;
+		yEnd = yEnd >= h ? h - 1 : yEnd;
+		for (let k = yStart; k <= yEnd; ++k) {
+			for (let l = 0; l < 2; ++l) {
+				neighbours += board[k][l];
+			}
+		}
+		for (let j = 0; j < w; ++j) {
+			let state = board[i][j];
+			out[i][j] = rules[state][neighbours - state];
+			let canAdd = j + 2 < w;
+			let canSub = j - 1 >= 0;
+			for (let m = yStart; m <= yEnd; ++m) {
+				canAdd && (neighbours += board[m][j + 2]);
+				canSub && (neighbours -= board[m][j - 1]);
+			}
+		}
+	}
+	return out;
 }
 
 function draw(
-  board: number[][],
-  ctx: CanvasRenderingContext2D,
-  aliveColor: string = "#FFFF00",
+	board: number[][],
+	ctx: CanvasRenderingContext2D,
+	aliveColor: string = "#FFFF00",
 ): void {
-  const h = board.length;
-  const w = board[0].length;
+	const h = board.length;
+	const w = board[0].length;
 
-  let oldStyle = ctx.fillStyle;
+	let oldStyle = ctx.fillStyle;
 
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  ctx.fillStyle = aliveColor;
-  ctx.beginPath();
-  for (let i = 0; i < h; ++i) {
-    for (let j = 0; j < w; ++j) {
-      let state = board[i][j];
-      if (!state) continue;
-      ctx.rect(j, i, 1, 1);
-    }
-  }
-  ctx.fill();
-  ctx.fillStyle = oldStyle;
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.fillStyle = aliveColor;
+	ctx.beginPath();
+	for (let i = 0; i < h; ++i) {
+		for (let j = 0; j < w; ++j) {
+			if (!board[i][j]) continue;
+			ctx.rect(j, i, 1, 1);
+		}
+	}
+	ctx.fill();
+	ctx.fillStyle = oldStyle;
 }
 
 function glider(board: number[][], x: number, y: number): void {
-  board[y][x + 1] = 1;
-  board[y + 1][x + 2] = 1;
-  board[y + 2][x] = 1;
-  board[y + 2][x + 1] = 1;
-  board[y + 2][x + 2] = 1;
+	board[y][x + 1] = 1;
+	board[y + 1][x + 2] = 1;
+	board[y + 2][x] = 1;
+	board[y + 2][x + 1] = 1;
+	board[y + 2][x + 2] = 1;
 }
 
 function clamp(x: number, min: number, max: number): number {
-  return Math.min(Math.max(x, min), max);
+	return Math.min(Math.max(x, min), max);
 }
 
 function clearBoard(board: number[][]): void {
-  for (let i = 0; i < board.length; ++i) {
-    for (let j = 0; j < board[i].length; ++j) {
-      board[i][j] = 0;
-    }
-  }
+	for (let i = 0; i < board.length; ++i) {
+		for (let j = 0; j < board[i].length; ++j) {
+			board[i][j] = 0;
+		}
+	}
 }
